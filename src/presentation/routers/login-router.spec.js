a88ff69013908cfc16ace725cad7a0d1998d1902
anthonyreis <matheus.anthony1@gmail.com>
2021-08-21 12:54:28 -0300

    move classes to their own files

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,40 +1,6 @@
-class LoginRouter {
-    route (httpRequest) {
-        if (!httpRequest || !httpRequest.body) {
-            return HttpResponse.serverError();
-        }
-        const { email, password } = httpRequest.body;
-        if (!email) {
-            return HttpResponse.badRequest('email');
-        }
+const LoginRouter = require('./login-router');
+const MissingParamError = require('../helpers/missing-param-error');
 
-        if (!password) {
-            return HttpResponse.badRequest('password');
-        }
-    }
-}
-
-class HttpResponse {
-    static badRequest(paramName) {
-        return {
-            statusCode: 400,
-            body: new MissingParamError(paramName)
-        };
-    }
-
-    static serverError () {
-        return {
-            statusCode: 500
-        };
-    }
-}
-
-class MissingParamError extends Error {
-    constructor(paramName) {
-        super(`Missing param: ${paramName}`);
-        this.name = 'MissingParamError';
-    }
-}
 describe('Login Router', () => {
     test('Should return 400 if no email is provided', () => {
         const sut = new LoginRouter();
